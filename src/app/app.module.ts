7f9c6e0e625824077e3f7af8c84274086ef3472d
Walace Silva <wsilva.emp@gmail.com>
2020-10-01 18:22:29 -0300

    [improvements] adjustments on header and navigation components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { HomeModule } from './modules/home/home.module';
 import { DashboardModule } from './modules/dashboard/dashboard.module';
 import { HeaderPrimaryComponent } from './components/header-primary/header-primary.component';
 import { SideNavigationComponent } from './components/side-navigation/side-navigation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,10 +19,11 @@ import { SideNavigationComponent } from './components/side-navigation/side-navig
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     NgbModule,
+    AppRoutingModule,
     HomeModule,
     DashboardModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
